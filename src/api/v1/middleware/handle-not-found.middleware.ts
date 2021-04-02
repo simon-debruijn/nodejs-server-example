c895c2781a6a94c0e899ff4898fec542579be23d
Simon de Bruijn <simon.debruijn@publiq.be>
2021-04-02 15:45:33 +0200

    Clean up handle not found

--- src/api/v1/middleware/handle-not-found.middleware.ts
+++ src/api/v1/middleware/handle-not-found.middleware.ts
@@ -5,7 +5,5 @@ export const handleNotFound = (
   response: Response,
   next: NextFunction
 ) => {
-  const message = 'Resource not found';
-
-  response.status(404).send(message);
+  response.status(404).send({ error: 'Resource not found' });
 };
