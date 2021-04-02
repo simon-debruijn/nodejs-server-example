74bd208339307b0c3818d812b71e7328b398e8a2
Simon de Bruijn <simon.debruijn@publiq.be>
2021-04-02 15:30:09 +0200

    Add validation to user class

--- src/api/v1/users/user.ts
+++ src/api/v1/users/user.ts
@@ -1,16 +1,40 @@
+import {
+  validateSync,
+  IsEmail,
+  IsString,
+  MinLength,
+  IsDefined,
+  ValidationError,
+} from 'class-validator';
+
 class User {
   _id: string;
+
+  @IsDefined()
+  @IsString()
   name: string;
+
+  @IsDefined()
+  @IsEmail()
   email: string;
+
+  @IsDefined()
+  @IsString()
+  @MinLength(6)
   password: string;
+
   tokens: string[] = [];
 
-  constructor(_id: string, name: string, email: string, password: string) {
-    this._id = _id;
+  constructor(name: string, email: string, password: string) {
+    this._id = Math.random().toString();
     this.name = name;
     this.email = email;
     this.password = password;
   }
+
+  static validate = (user: User): ValidationError[] => {
+    return validateSync(user);
+  };
 }
 
 export { User };
