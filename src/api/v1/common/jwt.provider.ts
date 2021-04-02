f5e7569f0464b8e4e0395ef0c4c81fd1fb1d819f
Simon de Bruijn <simon.debruijn@publiq.be>
2021-04-02 15:31:37 +0200

    Use cleaner syntax in jwt.provider

--- src/api/v1/common/jwt.provider.ts
+++ src/api/v1/common/jwt.provider.ts
@@ -2,13 +2,11 @@ import { User } from '../users/user';
 import jwt, { Secret } from 'jsonwebtoken';
 
 class JwtProvider {
-  static generateAuthToken = (user: User) => {
+  static generateAuthToken = ({ _id }: User) => {
     try {
-      const token = jwt.sign(
-        { _id: user['_id'].toString() },
-        process.env.JWT_SECRET as Secret,
-        { expiresIn: '1 day' }
-      );
+      const token = jwt.sign({ _id }, process.env.JWT_SECRET as Secret, {
+        expiresIn: '1 day',
+      });
       return token;
     } catch (error) {
       throw new Error(error.message);
