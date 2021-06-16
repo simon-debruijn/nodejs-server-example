31d66078af25ed2e4ad0e4beab3b07151784abbc
Simon de Bruijn <simon.debruijn@publiq.be>
2021-06-16 17:58:31 +0200

    Remove comments

--- src/api/v1/users/UsersRouter.ts
+++ src/api/v1/users/UsersRouter.ts
@@ -6,19 +6,15 @@ const usersRepository = UsersInMemoryRepository.instance;
 const usersController = new UsersController(usersRepository);
 const usersRouter = express.Router();
 
-// CREATE
 usersRouter.post('/', usersController.addUser);
 usersRouter.post('/login', usersController.loginUser);
 
-// READ
 usersRouter.get('/', usersController.getUsers);
 usersRouter.get('/:id', usersController.getUserById);
 
-// UPDATE
 usersRouter.patch('/', usersController.updateUsersByIds);
 usersRouter.patch('/:id', usersController.updateUserById);
 
-// DELETE
 usersRouter.delete('/', usersController.deleteUsersByIds);
 usersRouter.delete('/:id', usersController.deleteUserById);
 
