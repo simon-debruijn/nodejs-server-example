8227087cf611f60d5d3eb223d574c3110284fc98
Simon de Bruijn <simon.debruijn@publiq.be>
2021-06-17 13:47:29 +0200

    Return strings when updating or deleting multiple items

--- src/api/v1/common/RepositoryInterface.ts
+++ src/api/v1/common/RepositoryInterface.ts
@@ -1,14 +1,14 @@
 import { ValidationErrorResponse } from './ValidationErrorResponseInterface';
 
 interface Repository<T> {
-  addOne(newInstance: T): Promise<T | ValidationErrorResponse>;
-  addMany(newInstances: T[]): Promise<T[]>;
+  addOne(newInstance: any): Promise<T | ValidationErrorResponse>;
+  addMany(newInstances: any[]): Promise<T[] | ValidationErrorResponse>;
   findOneById(id: string): Promise<T | undefined>;
   find(properties?: Partial<T>): Promise<T[]>;
   deleteOneById(id: string): Promise<T | undefined>;
-  deleteManyByIds(ids: string[]): Promise<T[]>;
+  deleteManyByIds(ids: string[]): Promise<string>;
   updateOneById(id: string, properties: Partial<T>): Promise<T | undefined>;
-  updateManyByIds(ids: string[], properties: Partial<T>): Promise<T[]>;
+  updateManyByIds(ids: string[], properties: Partial<T>): Promise<string>;
 }
 
 export type { Repository };
